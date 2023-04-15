99a11aaed4e1ebf8b2f15ed680c73c2e29477ad2
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-04-16 04:33:47 +0530

    changed publlic

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,8 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://securetoken.googleapis.com" />
         <link rel="preconnect" href="https://identitytoolkit.googleapis.com" />
+        <link rel='apple-touch-icon' href='/icon-192x192.png' />
+        <link rel='icon' href='/icon-192x192.png' />
       </Head>
 
       <AppContextProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel='manifest' href='/manifest.json' />
-        <link rel='apple-touch-icon' href='/icon-192x192.png' />
+
         <meta name="theme-color" content="#141414" />
-        <link rel='icon' href='/icon.png' />
       </Head>
       <body>
         <div id='message-modal'></div>
