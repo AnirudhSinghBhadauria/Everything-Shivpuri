6bf19a7e50668d29431a2999c04b57ef6284f7a9
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-04-17 01:32:47 +0530

    done something

--- pages/_app.js
+++ pages/_app.js
@@ -7,16 +7,6 @@ import AppContextProvider from "../store/AppContextProvider";
 export default function App({ Component, pageProps }) {
   return (
     <Fragment>
-      <Head>
-        <title>Everything Shivpuri</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://securetoken.googleapis.com" />
-        <link rel="preconnect" href="https://identitytoolkit.googleapis.com" />
-        <link rel='apple-touch-icon' href='/icon-192x192.png' />
-        <link rel='icon' href='/icon-192x192.png' />
-      </Head>
-
       <AppContextProvider>
         <Layout>
           <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel='manifest' href='/manifest.json' />
-
+        <title>Everything Shivpuri</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://securetoken.googleapis.com" />
+        <link rel="preconnect" href="https://identitytoolkit.googleapis.com" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <link rel="icon" href="/icon-192x192.png" />
         <meta name="theme-color" content="#141414" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
-        <div id='message-modal'></div>
+        <div id="message-modal"></div>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
